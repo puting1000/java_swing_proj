5f24af65f13b5e54f822e5516fe3569ec789901d
puting1000@gmail.com <gh12dyddn>
2018-05-30 17:33:41 +0900

    05/30

---
+++ src/bento/delivery/program/TableMain.java
@@ -0,0 +1,34 @@
+package bento.delivery.program;
+
+import javax.swing.*;
+
+public class TableMain {
+	// 메인에 테이블 출력
+	// 테이블 주문별 보기, 사원별보기
+	
+	private JTable peopleTable, orderTable;
+	
+	public TableMain() {
+		getTableMain();
+	}
+	
+	public void getTableMain(){
+		
+
+	}
+	
+	public JTable getTablePeople(){
+		if(orderTable==null){
+			// JTable peopleTable = new JTable( rowData, columnNames );
+		}
+		return peopleTable;
+	}
+	
+	public JTable getTableOrder(){
+		if(orderTable==null){
+			// JTable orderTable = new JTable( rowData, columnNames );
+		}
+		return orderTable;
+	}
+
+}
